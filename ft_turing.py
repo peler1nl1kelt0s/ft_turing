506aaa45d26de8c25385005867a8f7aa00107e1c
Muhammed Esad  Şeker <esdskr@Muhammed-MacBook-Pro.local>
2026-02-08 15:03:47 +0300

    add: printing the states is completed

--- ft_turing.py
+++ ft_turing.py
@@ -1,6 +1,8 @@
 import json
+import sys
 
 class Tape:
+	# !!! ALAN GENISLETMELERI KONTROLUNU YAPMAYI UNUTMA
 	def __init__(self, input_str, blank_char):
 		self.tape = list(input_str) if input_str else blank_char
 		self.blank_char = blank_char
@@ -33,14 +35,39 @@ class TuringMachine:
 		self.current_state = self.initial_state
 		self.input = input
 		self.tape = Tape(self.input, self.blank)
+		self.file = open("results.txt", "w")
+
+	def print_states(self):
+		count = 39 - int(len(self.name) / 2)
+		is_count_odd = count % 2 == 1
+		print("is_count_odd: ", is_count_odd)
+		self.file.write("*" * 80 + "\n")
+		self.file.write("*" + " " * 78 + "*\n")
+		self.file.write("*" + (count - 1 if is_count_odd==True else count) * " " + self.name + count * " " + "*\n")
+		self.file.write("*" + " " * 78 + "*\n")
+		self.file.write("*" * 80 + "\n")
+		self.file.write("Alphabet: [ " + ", ".join(self.alphabet) + " ]\n")
+		self.file.write("States: [ " + ", ".join(self.states) + " ]\n")
+		self.file.write("Initial: " + self.initial_state + "\n")
+		self.file.write("Finals: [ " + ", ".join(self.finals_state) + " ]\n")
+		for state, rules in self.transitions.items():
+			for r in rules:
+				self.file.write(
+					f"({state}, {r['read']}) -> "
+					f"({r['to_state']}, {r['write']}, {r['action']})\n"
+				)
+		self.file.write("*" * 80)
 
 	def step(self):
 		if self.current_state in self.finals_state:
 			print("Program finale geldi")
-			return 1
+			self.file.close()
+			return 1 
+		
+		# !!!!!!! HATA DURUMLARI KONTROLLERINI DAHA SONRASINDA PARSE KISMINDA 
+		# YAP KI DAHA SONRASINDA TEK TEK KONTROL ETME !!!!!!!!!!
 
 		current_char = self.tape.read()
-
 		if self.current_state not in self.transitions:
 			print("state, transitions icerisinde bulunamadi")
 			return 0
@@ -59,7 +86,6 @@ class TuringMachine:
 		while steps < max_steps:
 			code = self.step()
 			steps += 1
-			print("current state: ", self.current_state)			
 			if code == 2:
 				continue
 			elif code == 1:
@@ -72,8 +98,13 @@ class TuringMachine:
 
 		print("ERROR: sonsuz döngü tespit edildi")
 		return 0
+	
 
 def	read_json(jsonfile):
-	with open(jsonfile, 'r') as file:
-		data = json.load(file)
-	return data
+	try:
+		with open(jsonfile, 'r') as file:
+			data = json.load(file)
+		return data
+	except:
+		print("Please give valid json file!!")
+		sys.exit(1)
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import sys
 from ft_turing import TuringMachine, read_json
 
 def main():
-	try:
-		json_arg = read_json(sys.argv[1])
-		machine = TuringMachine(jsonfile=json_arg, input=sys.argv[2])
-		machine.run()
-	except:
-		print("Error Accured when read json file!! Please give me json file")
+	# JSON PARSE KONTROLU YAPILACAK
+	if len(sys.argv) != 3:
+		print("Usage: python main.py <json_file> <input>")
+		sys.exit(1)
 
+	json_arg = read_json(sys.argv[1])
+	machine = TuringMachine(jsonfile=json_arg, input=sys.argv[2])
+	machine.print_states()
+	machine.run()
 if __name__ == "__main__":
 	main()
