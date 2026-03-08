31716861cfa39797bc0d45c5aeb14247ccaac175
Muhammed Esad Seker <museker@k1m04s06.42istanbul.com.tr>
2026-03-08 17:47:29 +0300

    add: Parse is started

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from parse import parse_json
+from parse import import_json
 from ft_turing import run
 
 def main():
@@ -8,14 +8,13 @@ def main():
 	if len(sys.argv) != 3:
 		print("Usage: python main.py <json_file> <input>")
 		sys.exit(1)
-	input_str=sys.argv[2]
 	try:
 		file = open(sys.argv[1])
 		json_arg = json.loads(file.read())
 	except:
 		print("Error: when reading json file. Please give valid json")
 		exit(1)
-	machine = parse_json(json_arg=json_arg, input_string=sys.argv[2])
+	machine = import_json(json_arg=json_arg, input_string=sys.argv[2])
 	run(machine=machine)
 
 if __name__ == "__main__":
--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,6 @@
 from dataclass import TuringMachine
 
-def parse_json(json_arg: dict, input_string : str) -> TuringMachine:
+def import_json(json_arg: dict, input_string : str) -> TuringMachine:
     try:
         return TuringMachine(
             name=json_arg.get("name"),
@@ -14,3 +14,27 @@ def parse_json(json_arg: dict, input_string : str) -> TuringMachine:
         )
     except Exception:
         raise ValueError("Error when parsing json file")
+    
+def get_state(machine : TuringMachine, state : str):
+    try:
+        return machine.transitions.get(state)
+    except:
+        raise ValueError("Error: states are invalid")
+
+def parse_states(machine : TuringMachine):
+    map(lambda s: get_state(s), machine.states)
+
+def parse_alphabet(machine : TuringMachine):
+    map(lambda )
+    pass
+
+def parse_halt():
+    pass
+
+def parse(machine : TuringMachine):
+    try:
+        parse_states(machine=machine)
+        parse_alphabet()
+    except ValueError as e:
+        print(e)
+        return 0
